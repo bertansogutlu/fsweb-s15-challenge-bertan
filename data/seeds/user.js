a2e9bc2cc90389cc39a592cf436d357e4fa20d90
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-10 17:52:38 +0300

    seed datalari olusturuldu

--- data/seeds/user.js
+++ data/seeds/user.js
@@ -2,12 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
+
+const defRiddles = require('../../api/bilmeceler/bilmeceler-data')
+const defRoles = [{roles_name: 'user'},{roles_name: 'admin'}]
+const defUsers = [
+  {username: 'Defne', password: '$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq', role_id: 1},
+  {username: 'Bertan', password: '$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq', role_id: 2}
+]
+
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex('users').truncate()
+  await knex('roles').truncate()
+  await knex('riddles').truncate()
+
+  await knex('riddles').insert(defRiddles)
+  await knex('roles').insert(defRoles)
+  await knex('users').insert(defUsers) 
 };
