a2349a9e09d2e02c52eea106ff6a40845b6c69fb
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-11 02:55:39 +0300

    server test olusturuldu

--- api/server.test.js
+++ api/server.test.js
@@ -17,10 +17,94 @@ test('[0] Testler çalışır durumda]', () => {
 })
 
 describe('Server test',()=>{
+
   test("[1] Register payload dolu başarılı sonuç",async ()=>{
-    let sampleUser = {"username":"veysel12",password:"1234","rolename":"admin"};
-    const res = await supertest(server).post("/api/auth/register").send(sampleUser);
-    expect(res.status).toBe(201);
-    expect(res.body.user_id).toBeGreaterThan(0)
+    const user = {"username":"Pasa",password:"1234"};
+    const res = await supertest(server).post("/api/auth/register").send(user);
+    expect(res.status).toBe(200);
+    expect(res.body.password).not.toBeNull();
+    expect(res.body).toMatchObject({
+      "user_id": 3,
+      "username": "Pasa",
   })
+  })
+
+  test("[2] Register payload eksik başarılı sonuç",async ()=>{
+    const user = {"username":"Pasa"};
+    const res = await supertest(server).post("/api/auth/register").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toMatchObject({ message: "username ve password gereklidir" })
+  })
+
+  test("[3] Register payload eksik başarılı sonuç",async ()=>{
+    const user = {password:"1234"};
+    const res = await supertest(server).post("/api/auth/register").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toMatchObject({ message: "username ve password gereklidir" })
+  })
+
+  test("[3] Register alinmis isim başarılı sonuç",async ()=>{
+    const user = {username: 'Defne', password: '1234'};
+    const res = await supertest(server).post("/api/auth/register").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toMatchObject({ message: "username alınmış" })
+  })
+
+  test("[4] Login payload dolu başarılı sonuç",async ()=>{
+    const user = {username: 'Defne', password: '1234'};
+    const res = await supertest(server).post("/api/auth/login").send(user);
+    expect(res.status).toBe(200);
+    expect(res.body.token).not.toBeNull();
+    expect(res.body).toMatchObject({"message": "welcome, Defne"})
+  })
+
+  test("[5] Login payload eksik başarılı sonuç",async ()=>{
+    const user = {username: 'Defne'};
+    const res = await supertest(server).post("/api/auth/login").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toMatchObject({"message": "username ve password gereklidir"})
+  })
+
+  test("[6] Login payload eksik başarılı sonuç",async ()=>{
+    const user = {password: '1234'};
+    const res = await supertest(server).post("/api/auth/login").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toMatchObject({"message": "username ve password gereklidir"})
+  })
+
+  test("[7] Login payload eksik başarılı sonuç",async ()=>{
+    const user = {username: 'Def', password: '1234'};
+    const res = await supertest(server).post("/api/auth/login").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toMatchObject({"message": "geçersiz kriterler"})
+  })
+
+  test("[8] Login payload eksik başarılı sonuç",async ()=>{
+    const user = {username: 'Defne', password: '12345'};
+    const res = await supertest(server).post("/api/auth/login").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toMatchObject({"message": "geçersiz kriterler"})
+  })
+  
+  test("[9] Login doğru payload başarılı token",async ()=>{
+    const user = {username: 'Defne', password: '1234'};
+    const res = await supertest(server).post("/api/auth/login").send(user);
+    expect(res.status).toBe(200);
+    expect(res.body.token).not.toBeNull();
+  })
+
+  test("[10] Bilmeceler tokensiz açılmaz",async ()=>{
+    const res = await supertest(server).get("/api/bilmeceler");
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe("token gereklidir");
+  })
+
+  test("[6] Bilmeceler token ile listeleniyor",async ()=>{
+    const user = {username: 'Defne', password: '1234'};
+    const login = await supertest(server).post("/api/auth/login").send(user);
+    const res = await supertest(server).get("/api/bilmeceler").set("authorization",login.body.token);
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(3);
+  })
+
 })
