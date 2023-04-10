9ca17f0063de5dc0f9d40a8de407305232b9c51c
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-10 20:54:19 +0300

    bilmeceler router olusturuldu

--- api/bilmeceler/bilmeceler-router.js
+++ api/bilmeceler/bilmeceler-router.js
@@ -3,15 +3,25 @@ const router = require('express').Router();
 const bilmeceler = require('./bilmeceler-data');
 const bilmecelerModel = require('./bilmeceler-model');
 
-router.get('/', (req, res) => {
-  res.status(200).json();
-});
-
-router.get('/deneme', async(req, res, next) => {
+router.get('/', async(req, res, next) => {
   try {
-    res.status(200).json(await bilmecelerModel.create( {
-      "bilmece": "Bir kamyonu kim cift eliyle durdurabilir?"
-    }))
+    res.status(200).json(await bilmecelerModel.getAll())
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/id', async(req, res, next) => {
+  try {
+    res.status(200).json(await bilmecelerModel.getById(req.body.id))
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async(req, res, next) => {
+  try {
+    res.status(201).json(await bilmecelerModel.create(req.body))
   } catch (error) {
     next(error)
   }
